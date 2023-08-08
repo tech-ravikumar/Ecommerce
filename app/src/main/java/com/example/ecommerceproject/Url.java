40091301faf46c4cee4d6bce0b5ac8daab6253a8
Ravi <ravi182003kumar@gmail.com>
2023-08-08 15:37:48 +0530

    url

--- app/src/main/java/com/example/ecommerceproject/Url.java
+++ app/src/main/java/com/example/ecommerceproject/Url.java
@@ -1,7 +1,7 @@
 package com.example.ecommerceproject;
 
 public class Url {
-    public static String HOST = "http://192.168.1.28/";
+    public static String HOST = "http://192.168.1.5/";
     public static String BASEURL = HOST + "php_rest_api/";
 
 }
